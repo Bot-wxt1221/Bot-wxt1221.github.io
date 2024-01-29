954fc60a96be8c811e9f3a53b96f3f98ae520be8
wxt1221 <3264117476@qq.com>
2024-01-29 19:17:19 +0800

    fix permissions

--- auto-generate-issue.js
+++ auto-generate-issue.js
@@ -170,7 +170,6 @@ const gitalkInit = ({
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
       'Authorization': 'Bearer ' + config.token,
       'Accept': 'application/json',
-      'Content-Type': 'application/json;charset=UTF-8'
     },
     url: api,
     body: JSON.stringify(reqBody),
