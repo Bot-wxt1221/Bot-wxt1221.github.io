a8075950d3eee8a46fdccb0f381713044722d043
wxt1221 <3264117476@qq.com>
2024-01-29 19:03:50 +0800

    fix permissions

--- auto-generate-issue.js
+++ auto-generate-issue.js
@@ -90,7 +90,7 @@ console.log(link);
 const getIsInitByRequest = (id) => {
   const options = {
     headers: {
-      'authorization': 'Bearer ' + config.token,
+      'Authorization': 'Bearer ' + config.token,
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
       'Accept': 'application/json'
     },
@@ -167,7 +167,7 @@ const gitalkInit = ({
 
   const options = {
     headers: {
-      'authorization': 'Bearer ' + config.token,
+      'Authorization': 'Bearer ' + config.token,
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
       'Accept': 'application/json',
       'Content-Type': 'application/json;charset=UTF-8'
