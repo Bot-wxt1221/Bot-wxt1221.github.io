7ba81e9518897f6d527725a82d0f234f4eb3fa3b
wxt1221 <3264117476@qq.com>
2024-01-29 18:51:00 +0800

    fix permissions

--- auto-generate-issue.js
+++ auto-generate-issue.js
@@ -90,7 +90,7 @@ console.log(link);
 const getIsInitByRequest = (id) => {
   const options = {
     headers: {
-      'Authorization': 'Bearer ' + config.token,
+      'authorization': 'Bearer ' + config.token,
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
       'Accept': 'application/json'
     },
